d79dee44a689d5587ef38b234b30e9689c3f12a9
Diane <dianez@mit.edu>
2020-07-25 18:35:58 -0400

    Create dataloader at beginning of each epoch

--- train.py
+++ train.py
@@ -15,8 +15,7 @@ if __name__ == '__main__':
     videos_list = ["2020-03-15_19-27-56-f2472745", "2020-06-22_11-14-22-319eaf00", 
                    "2020-06-25_17-25-16_alexl_everyday-tyingshoelaces-189703d3"]
     dataset = GazeFrameDataset(data_path, videos_list, transform=transform)
-    dataloader = torch.utils.data.DataLoader(dataset)
-    print("Dataloader done")
+    print("Dataset done")
 
     # net
     net = SaliencyNet()
@@ -30,7 +29,10 @@ if __name__ == '__main__':
     print("Net setup")
 
     for epoch in range(10):
+        print("Epoch", epoch + 1)
         running_loss = 0.0
+        dataloader = torch.utils.data.DataLoader(dataset)
+        print("Dataloader done")
         for i, data in enumerate(dataloader, 0):
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
@@ -40,7 +42,7 @@ if __name__ == '__main__':
             optimizer.step()
 
             running_loss += loss.item()
-            if i%100 == 99:
+            if i%1000 == 999:
                 print('[%d, %5d] loss: %.10f' %
                     (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
