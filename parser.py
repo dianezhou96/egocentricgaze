69509abc7946fa87448682d6e31d85153cd0206f
Diane Y Zhou <dianez@visiongpu26.csail.mit.edu>
2020-08-06 03:33:10 -0400

    Added type for some parameters in parser.py. Added batch size parameter.

--- parser.py
+++ parser.py
@@ -6,9 +6,11 @@ parser.add_argument("--train_file", default="train.txt", help="File with list of
 parser.add_argument("--validation_file", default="validate.txt", help="File with list of videos for validation")
 parser.add_argument("--test_file", default="test.txt", help="File with list of videos for testing")
 
-parser.add_argument("--shifted_grids", default=False, help="True if using model with shifted grids")
-parser.add_argument("--gaussian_blur_size", default=3, help="Size of Gaussian blur")
-parser.add_argument("--N", default=5, help="Size of shifted grids")
+parser.add_argument("--shifted_grids", type=bool, default=False, help="True if using model with shifted grids")
+parser.add_argument("--gaussian_blur_size", type=int, default=3, help="Size of Gaussian blur")
+parser.add_argument("--N", default=5, type=int, help="Size of shifted grids")
 
-parser.add_argument("--learning_rate", default=0.01, help="Learning rate for training")
-parser.add_argument("--num_epochs", default=10, help="Number of epochs to train")
+parser.add_argument("--learning_rate", type=float, default=0.01, help="Learning rate for training")
+parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train")
+parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
+
